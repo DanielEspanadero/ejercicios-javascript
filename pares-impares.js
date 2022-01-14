618589691348ec986e79f9e7bf99087731dc953f
Daniel Españadero <developer@daniel-espanadero.com>
2022-01-14 13:38:55 +0100

    pares-impares

---
+++ pares-impares.js
@@ -0,0 +1,16 @@
+// Extrae los números pares e impares del array números.git 
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+let pares = [];
+let impares = [];
+
+for (i = 0; i <= numeros.length; i++) {
+    if (i % 2 === 0) {
+        pares.push(i);
+    } else {
+        impares.push(i)
+    }
+};
+
+console.log(`Los números pares son ${pares}`);
+console.log(`Los números impares son ${impares}`);
